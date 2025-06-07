2a5f33c3677782a92546558820adce36694cee19
Adityapanji03 <adityapanjisaja@gmail.com>
2025-06-07 08:03:37 -0700

    Deskripsi singkat perubahan saya

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,2 @@
 import './bootstrap';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import Finance from './finance'; // Import komponen finance
 
-// Render ke elemen dengan id 'app'
-const container = document.getElementById('app');
-const root = createRoot(container);
-root.render(<Finance />);
